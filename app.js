e8104c1799b187536678c2c0dc1441b59246e81c
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-03-24 21:51:11 +0530

    jwt & cookies

--- app.js
+++ app.js
@@ -3,42 +3,45 @@ require('dotenv').config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const cookieParser = require("cookie-parser");
-const connectMongo = require("connect-mongo")
 const session = require("express-session");
-const connectDB = require("./server/config/db")
-const app = express();
-const port = 3000 || process.env.PORT;
+const mongoose = require("mongoose");
 
-//db connect
+// import connect-mongo and pass session
+const MongoStore = require("connect-mongo"); 
+const connectDB = require("./server/config/db");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
 connectDB();
 
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
-const  MongoStore = connectMongo(session); 
+const sessionStore = MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
+    mongooseConnection: mongoose.connection,
+});
 
+// Session middleware
 app.use(session({
-    secret : "keyboard cat",
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({
-        mongoUrl: process.env.MONGODB_URI
-    })
-    // cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
-}))
+    store: sessionStore,
+}));
 
-//Template Engine
+// Template Engine
 app.use(expressLayouts);
-//app.use(express.static('public'));
 app.set("layout", "./layouts/main");
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
+// Define routes
 app.use("/", require("./server/routes/main"));
 app.use("/", require("./server/routes/admin"));
 
-app.listen(port, ()=>{
-    console.log(`App is listening on ${port}`)
-})
-
+app.listen(port, () => {
+    console.log(`App is listening on ${port}`);
+});
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,10 +2,28 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
-const Bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const adminLayout = "../views/layouts/admin"
+const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
+
+//check-login
+const authMiddleware = (req, res, next)=>{
+    const token = req.cookies.token;
+
+    if(!token){
+        res.status(404).json({ message: "Unauthorized" });
+    }
+
+    try{
+        const decode = jwt.verify(token, jwtSecret);
+        req.userId = decode.userId;
+        next();
+    } catch{
+        res.status(404).json({ message: "Unauthorized" });
+    }
+}
 
 //admin - login page
 router.get('/admin', async (req, res) => {
@@ -21,8 +39,63 @@ router.get('/admin', async (req, res) => {
   }
 });
 
-//POST - admin
+//admin - check login
 router.post('/admin', async (req, res) => {
+      try {
+        const {username, password } = req.body;
+
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(401).json({message: "Invalid Credentials."})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({message: "Invalid Credentials."})
+        }
+
+        const token = jwt.sign({userId: user._id}, jwtSecret );
+        res.cookie("token", token, {httpOnly: true});
+        res.redirect("/dashboard");
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    
+    });
+
+    //dashboard
+router.get('/dashboard', authMiddleware, async (req, res) => {
+
+    try {
+        const data = await Post.find();
+        res.render("/admin/dashboard", {
+            locals,
+        })
+    } catch (error) {
+        
+    }
+    
+    
+    });
+
+// router.get('', async (req, res) => {
+//   const locals = {
+//     title: "NodeJs Blog",
+//     description: "Simple Blog created with NodeJs, Express & MongoDb."
+//   }
+
+//   try {
+//     const data = await Post.find();
+//     res.render('index', { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// });
+
+//POST - admin register
+router.post('/register', async (req, res) => {
     try {
         const {username, password} = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -31,7 +104,7 @@ router.post('/admin', async (req, res) => {
             const user = await User.create({ username, password: hashedPassword });
             res.status(201).json({ message: "User Created!", user});
         } catch{
-            if(error.code = 11000){
+            if(error.code === 11000){
                 res.status(409).json({ message: "User already in use"});
             }
             res.status(500).json({ message: "Internal Server Error"});
@@ -41,6 +114,5 @@ router.post('/admin', async (req, res) => {
       console.log(error);
     }
   });
-
 
 module.exports = router;
