48e33051af3706e8d6cd92dfb75c26bd3f589580
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-03-24 17:54:33 +0530

    admin

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 const express = require("express");
-const expressLayout = require("express-ejs-layouts");
-
+const expressLayouts = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const connectMongo = require("connect-mongo")
+const session = require("express-session");
 const connectDB = require("./server/config/db")
-
 const app = express();
 const port = 3000 || process.env.PORT;
 
@@ -14,14 +15,28 @@ connectDB();
 app.use(express.urlencoded({extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
+
+const  MongoStore = connectMongo(session); 
+
+app.use(session({
+    secret : "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongoUrl: process.env.MONGODB_URI
+    })
+    // cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}))
 
 //Template Engine
-app.use(expressLayout);
-app.use(express.static('public'));
+app.use(expressLayouts);
+//app.use(express.static('public'));
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs")
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(port, ()=>{
     console.log(`App is listening on ${port}`)
